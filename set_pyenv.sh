ee826990221689fd643eff769a9f80d69b7942c7
cat <31work@first.com>
2019-10-30 11:44:10 +0800

    first file

---
+++ set_pyenv.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
---
+++ set_virtualenv.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+source ~/.bash_profile
